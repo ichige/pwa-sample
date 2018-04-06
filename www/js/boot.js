0a69ee0d947ee6e3c4553dfcda16c12c452670c6
ichige <ichige@emyus.jp>
2018-04-06 21:27:24 +0900

    サンプル完成かな？

---
+++ www/js/boot.js
@@ -0,0 +1,8 @@
+'use strict'
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/js/service-worker.js')
+    .then(function () {
+      console.log('Service Worker Registered!');
+    });
+}
---
+++ www/js/service-worker.js
@@ -0,0 +1,48 @@
+var cacheName = 'tempConverterShell';
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/main.js',
+  '/css/main.css'
+];
+
+self.addEventListener('install', function (e) {
+  console.log('Service Worker instll event!');
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(function (cache) {
+        console.log('Service Worker cachinge app shell!');
+        return cache.addAll(filesToCache);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('Service Worker activating!');
+  e.waitUntil(
+    caches.keys()
+      .then(function (keyList) {
+        return Promise.all(keyList.map(function (key) {
+          if (key !== cacheName) {
+            console.log('Service Worker removing old cache!');
+            return caches.delete(key);
+          }
+        }));
+      }
+    )
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function (e) {
+  console.log('Service Worker fetching!', e.request.url);
+  e.respondWith(
+    caches.match(e.request)
+      .then(function (response) {
+        return response || fetch(e.request);
+      }
+    )
+  );
+});
